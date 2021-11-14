032f584967b42685ade0be5792f7a4a5125d1311
clide7029 <70732345+clide7029@users.noreply.github.com>
2021-11-13 16:30:01 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const sample = ['R', 'G', "B", "Y"];
+
+function light(color) {
+
+}
+
+
+
+function gameLoop() {
+    red = document.getElementById("#redSq");
+    blue = document.querySelector(".blue");
+    green = document.querySelector(".green");
+    yellow = document.querySelector(".yellow");
+
+
+    console.log("here");
+    red.classList = "lightRed";
+}
