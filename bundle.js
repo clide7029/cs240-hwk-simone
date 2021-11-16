378d1441cf187540b6e4be3d7892a07ba1e5c439
clide7029 <70732345+clide7029@users.noreply.github.com>
2021-11-15 22:00:29 -0800

    added comments

--- bundle.js
+++ bundle.js
@@ -185,8 +185,13 @@ process.chdir = function (dir) {
 process.umask = function() { return 0; };
 
 },{}],2:[function(require,module,exports){
+//axios package
 const axios = require("./node_modules/axios");
 
+/* 
+Button Object
+@param color of the button
+ */
 class Button {
     constructor(color) {
         this.color = color;
@@ -194,26 +199,36 @@ class Button {
         this.element = document.getElementById(`${color}Sq`);
     }
 
+    //lights up the button
     light() {
         this.element.className = `light${this.color}`;
     }
+
+    //turns off button light
     dim() {
         this.element.className = `${this.color}`;
     }
+
+    //draws border around button to indicate mouse is hovering
     drawBorder() {
         this.element.classList.add("hover");
     }
+
+    //remove the button border
     removeBorder() {
         this.element.classList.remove("hover");
     }
+
+    //play the sound specific to this button
     sound() {
         let noise = new Audio(`./sounds/${this.color}.wav`);
         noise.play();
     }
 }
 
-const list = ['R', 'G', "B", "Y", 'R', 'G', "B", "Y", 'R', 'G', "B", "Y"];
-
+/* 
+Game Object
+ */
 class Game {
     constructor() {
         this.red = new Button('red');
@@ -225,47 +240,80 @@ class Game {
         this.url = 'http://cs.pugetsound.edu/~dchiu/cs240/api/simone/';
     }
 
+    /* 
+    Query API for start and solution sequences
+    @param rounds number of rounds to play
+     */
     async getSequence(rounds) {
-        // let HEAD = {
-        //     headers: {
-        //         httpHeader: 'v',
-        //     },
-        // };
         try {
             let startResponse = await axios.get(this.url + "?cmd=start");
             let roundResponse = await axios.get(this.url + `?cmd=getSolution&rounds=${rounds}`);
             this.startSequence = startResponse.data.sequence;
             this.roundSequence = roundResponse.data.key;
-            console.dir(this.roundSequence);
-            console.dir(this.startSequence);
         } catch (error) {
             console.log(error);
+            return false;
+        }
+        return true;
+    }
+
+    /* 
+    start game
+    @param rounds number of rounds to play
+     */
+    async start(rounds) {
+        this.sub = [];
+        this.checked = 0;
+        this.completed = 1;
+        let flag = await this.getSequence(rounds);
+        if (flag) {
+            this.addButtonListeners();
+            parseColorSequence(this.startSequence, T_BLINK);
+            await pause(T_START);
+            this.playRound(this.completed);
         }
     }
 
-    async start(rounds) {
-        //get list from API
-        // this.sequence = list.slice(0, rounds);
-        await this.getSequence(rounds);
-        this.completed = 1;
-        this.sub = [];
-        this.checked = 0;
-        this.addButtonListeners();
-
-        console.dir(this.startSequence);
-        parseColorSequence(this.startSequence, T_GREET);
-        await pause(T_START);
-        this.playRound(1);
-    }
-
-    playRound(round) {
+    /* 
+    play the specified ran by incrementing through
+    @param round which round to play
+     */
+    async playRound(round) {
         for (let i = 0; i <= round; i++) {
             this.sub = this.roundSequence.slice(0, i);
-            console.dir(this.sub);
             parseColorSequence(this.sub, T_BUTTON);
         }
     }
 
+    /* 
+    checks user input matches sequence
+    @param rounds number of rounds to play
+     */
+    checkInput(alias) {
+            if (this.roundSequence[this.checked] != alias) {
+                this.lose();
+                return false;
+            }
+            this.checked++;
+            this.status.innerHTML = `So far so good! ${this.sub.length - this.checked} more to go!`;
+
+            if (this.checked == this.roundSequence.length) {
+                this.win();
+                return false;
+            }
+
+            if (this.checked == this.sub.length) {
+                this.completed++;
+                this.checked = 0;
+                this.nextRound(this.completed);
+            }
+            return true;
+
+        }
+        /* 
+        move to the next round
+        @param round the round
+        */
     async nextRound(round) {
         this.status.innerHTML = "Good job! Prepare for next round.";
         playSound('nextRound');
@@ -275,40 +323,22 @@ class Game {
         this.playRound(round)
     }
 
-    checkInput(alias) {
-        if (this.roundSequence[this.checked] != alias) {
-            this.lose();
-            return false;
-        }
-        this.checked++;
-        this.status.innerHTML = `So far so good! ${this.sub.length - this.checked} more to go!`;
-
-        if (this.checked == this.roundSequence.length) {
-            this.win();
-            return false;
-        }
-
-        if (this.checked == this.sub.length) {
-            this.completed++;
-            this.checked = 0;
-            this.nextRound(this.completed);
-        }
-        return true;
-
-    }
-
+    //lose the game
     lose() {
+        playSound('wrong');
         playSound('lose');
         this.body.style.background = 'hotpink';
         this.status.innerHTML = "Incorrect. You Lose!"
     }
 
+    //win the game
     win() {
         playSound('win');
         this.body.style.background = 'deepskyblue';
         this.status.innerHTML = "Congratulations! You Win!";
     }
 
+    //adds all listeners to the buttons
     addButtonListeners() {
         addListeners(this.red);
         addListeners(this.blue);
@@ -317,19 +347,31 @@ class Game {
     }
 }
 
-var game = new Game();
-const T_GREET = 120;
+
+// delay constants
+const T_BLINK = 120;
 const T_START = 4000;
 const T_BUTTON = 400;
 const T_ROUND = 800;
 
+
+//start game
+var game = new Game();
 play = document.getElementById("play");
 play.addEventListener("click", function() {
     let rounds = document.getElementById("rounds").value;
+    if (rounds < 1 || isNaN(rounds)) {
+        rounds = 10;
+    }
+
     game.start(rounds);
 
 });
 
+/* 
+dynamically add all listeners to a button object
+@param node the button to add listeners to
+ */
 function addListeners(node) {
     node.element.addEventListener("mouseover", function() {
         node.drawBorder();
@@ -347,15 +389,16 @@ function addListeners(node) {
         node.dim();
         node.sound();
         game.checkInput(node.alias);
-        await pause(T_BUTTON);
     });
 }
 
-
-
+/* 
+plays given color sequence
+@param sequence of colors to play
+@param wait time to wait between pushing buttons
+ */
 async function parseColorSequence(sequence, wait) {
     for (let i = 0; i < sequence.length; i++) {
-
         switch (sequence[i]) {
             case 'R':
                 game.red.light();
@@ -382,9 +425,14 @@ async function parseColorSequence(sequence, wait) {
                 game.yellow.dim();
                 break;
         }
+        await pause(wait);
     }
 }
 
+/* 
+plays sound 
+@param effect which sound to play
+ */
 function playSound(effect) {
 
     switch (effect) {
@@ -399,10 +447,14 @@ function playSound(effect) {
     }
 }
 
-
-
+/* 
+delay program
+@param t specified time to wait in ms
+ */
 function pause(t) {
-    return new Promise((resolve) => setTimeout(resolve, t));
+    return new Promise(function(resolve) {
+        return setTimeout(resolve, t);
+    });
 }
 },{"./node_modules/axios":3}],3:[function(require,module,exports){
 module.exports = require('./lib/axios');
